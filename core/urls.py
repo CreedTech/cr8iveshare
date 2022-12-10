1bca9c6503936dc57904c7168980f57da37ea37b
creedtech <ayodeleayolaoke4@gmail.com>
2022-12-10 18:08:25 +0100

    added contact and about page but mobile toggle not working yet

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from .views import (
-    index
+    index,
+    about,
+    contact
 )
 
 app_name = 'core'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('/about', about, name='about'),
+    path('/contact', contact, name='contact'),
 ]
--- core/views.py
+++ core/views.py
@@ -6,3 +6,13 @@ from django.shortcuts import render
 def index(request):
     template_name = "index.html"
     return render(request, template_name)
+
+
+def about(request):
+    template_name = "about.html"
+    return render(request, template_name)
+
+
+def contact(request):
+    template_name = "contact.html"
+    return render(request, template_name)
