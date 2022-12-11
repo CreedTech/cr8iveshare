2f97357155a6cc052f389730ae3aaca20d6fe47e
creedtech <ayodeleayolaoke4@gmail.com>
2022-12-11 13:38:06 +0100

    uploaded files from template

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_admin = models.BooleanField('Admin', default=False)
+    profile_image = models.ImageField(
+        default='dhttps://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png',
+        upload_to='portfolio/',
+        validators=[FileExtensionValidator(['jpg', 'png', 'jpeg'])]
+    )
+    date_created = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
--- core/urls.py
+++ core/urls.py
@@ -9,6 +9,6 @@ app_name = 'core'
 
 urlpatterns = [
     path('', index, name='index'),
-    path('/about', about, name='about'),
-    path('/contact', contact, name='contact'),
+    path('about', about, name='about'),
+    path('contact', contact, name='contact'),
 ]
