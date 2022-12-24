d6d172a8f5a9e1f12e03de8b94a135e2714c1f88
creedtech <ayodeleayolaoke4@gmail.com>
2022-12-24 09:14:50 +0100

    changed account designs and logos

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Video, Comment, Channel, Like, Dislike, Video_View, Channel_Subscription, Category
+from .models import Video, Comment, Channel, Like, Dislike, Video_View, Channel_Subscription, Category, PageContents
 # Register your models here.
 
 admin.site.register([Video, Comment, Channel, Like,
-                     Dislike, Video_View, Channel_Subscription, Category])
+                     Dislike, Video_View, Channel_Subscription, Category, PageContents])
